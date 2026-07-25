9e561003e678a81ea35d1e45b3f4dce8a484bbe7
Yuvaraj B Talawar <yuvarajbtalawar99@gmail.com>
2026-07-25 16:20:59 +0530

    Save local progress before merging

--- backend/src/scratch_db_test.js
+++
@@ -1,1 +0,0 @@
-// Temporary diagnostic file cleared
--- backend/src/utils/context.util.ts
+++
@@ -1,8 +0,0 @@
-import { AsyncLocalStorage } from 'async_hooks';
-
-export interface UserContext {
-  userId: string;
-  role: string;
-}
-
-export const contextStorage = new AsyncLocalStorage<UserContext>();
